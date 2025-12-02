13e3f1a9fa40735c77e56c155b7982879b59c256
fiorella <fmilla1000@gmail.com>
2025-12-02 01:12:23 +0100

    set up of db in backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import register_routes
 from dotenv import load_dotenv
 
 load_dotenv()
 
+from routes import register_routes
+from database import Base, engine
+import models
+
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
+
+    Base.metadata.create_all(bind=engine)
+
     register_routes(app)
     return app
 
