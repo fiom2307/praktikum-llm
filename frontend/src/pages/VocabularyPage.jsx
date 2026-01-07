f70827585b98bc0b7c4d260f3bb4cd64da45587c
fiorella <fmilla1000@gmail.com>
2026-01-07 22:30:29 +0100

    fixed pizzacount never used

--- frontend/src/pages/VocabularyPage.jsx
+++ frontend/src/pages/VocabularyPage.jsx
@@ -14,7 +14,6 @@ import MascotOverlay from "../components/MascotOverlay";
 function VocabularyPage() {
     const { 
         updatePizzaCount, 
-        pizzaCount, 
         currentCostumeId, 
         tutorialProgress, 
         completeTutorialContext 
