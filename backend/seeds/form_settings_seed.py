102d6118abe5ba8e901b6ece1c7a3f5e9efaf1ec
fiorella <fmilla1000@gmail.com>
2026-01-15 20:02:03 +0100

    added links to the seed from form

--- backend/seeds/form_settings_seed.py
+++ backend/seeds/form_settings_seed.py
@@ -7,9 +7,9 @@ def seed_form_settings():
     default_settings = {
         "id": 1,
         "pretest_enabled": False,
-        "pretest_url": "https://docs.google.com/forms/d/e/1FAIpQLSet9c40nwjKGZ6XGJ2tPWj0hW_960yl0xfAYSaIRm8-pTfaIw/viewform?usp=pp_url&entry.1458977373=",
+        "pretest_url": "https://docs.google.com/forms/d/e/1FAIpQLScH3j7FehXKkRrXw_Xdd-nbVdgHYghE8r8G_xB4adRKt8pKcg/viewform?usp=pp_url&entry.1646617742=",
         "posttest_enabled": False,
-        "posttest_url": "https://docs.google.com/forms/d/e/1FAIpQLSet9c40nwjKGZ6XGJ2tPWj0hW_960yl0xfAYSaIRm8-pTfaIw/viewform?usp=pp_url&entry.1458977373=",
+        "posttest_url": "https://docs.google.com/forms/d/e/1FAIpQLScjRTjcD2RVHFdEYZF2-rMsCglGqn09YGpv2NHiLDLHVqbvQQ/viewform?usp=pp_url&entry.170677882=",
     }
 
     settings = db.query(FormSettings).filter(FormSettings.id == 1).first()
